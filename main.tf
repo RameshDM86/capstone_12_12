5f2d7f507a85e1bcaf3a693afb96de8542f29744
Ramesh DM <ramesha0486@gmail.com>
2023-12-12 09:41:18 +0800

    first commit

--- main.tf
+++ main.tf
@@ -57,11 +57,62 @@ resource "aws_s3_bucket_acl" "my-static-website" {
   acl    = "public-read"
 }
 
-
-
-
 # s3 static website url
 
-output "website_url" {
-  value = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.us-east-1.amazonaws.com"
+
+locals {
+  origin_id = "s3origin"
+}
+
+resource "aws_s3_bucket" "static_web" {
+
+  bucket = "${var.prefix}-s3-bkt"
+}
+
+resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
+  bucket = aws_s3_bucket.static_web.id
+  policy = data.aws_iam_policy_document.default.json
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  
+  origin {
+    domain_name              = aws_s3_bucket.static_web.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = local.origin_id
+  }
+
+  aliases = var.aliases
+
+  enabled             = true
+  comment             = "Static Website using S3 and Cloudfront OAC"
+  default_root_object = "index.html"
+  web_acl_id = var.web_acl_id
+
+  default_cache_behavior {
+    cache_policy_id        = data.aws_cloudfront_cache_policy.example.id
+    allowed_methods        = var.allowed_methods
+    cached_methods         = var.cached_methods
+    target_origin_id       = local.origin_id
+    viewer_protocol_policy = var.viewer_protocol_policy
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = var.acm_certificate_arn
+    ssl_support_method = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "${aws_s3_bucket.static_web.id}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,27 @@
-output "s3_bucket_name" {
-    value = aws_s3_bucket.my-static-website.id
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "cloudfront_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.static_web.id
+}
+
+output "cf_domain" {
+  value = module.static_web_stack.cloudfront_domain
+}
+
+output "cf_id" {
+  value = module.static_web_stack.cloudfront_id
+}
+
+output "bucket_name" {
+  value = module.static_web_stack.bucket_name
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.us-east-1.amazonaws.com"
 }
