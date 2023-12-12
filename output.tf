08d048dc00593cd48ed5dd67a460dcac03cf7d61
Ramesh DM <ramesha0486@gmail.com>
2023-12-12 10:19:47 +0800

    second commit

--- output.tf
+++ output.tf
@@ -10,17 +10,17 @@ output "bucket_name" {
   value = aws_s3_bucket.static_web.id
 }
 
-output "cf_domain" {
-  value = module.static_web_stack.cloudfront_domain
-}
+#output "cf_domain" {
+#  value = resource.static_web_stack.cloudfront_domain
+#}
 
-output "cf_id" {
-  value = module.static_web_stack.cloudfront_id
-}
+#output "cf_id" {
+# value = resource.static_web_stack.cloudfront_id
+#}
 
-output "bucket_name" {
-  value = module.static_web_stack.bucket_name
-}
+#output "bucket_name_another" {
+#   value = aws.static_web_stack.bucket_name_another
+#}
 
 output "website_url" {
   value = "http://${aws_s3_bucket.my-static-website.bucket}.s3-website.us-east-1.amazonaws.com"
